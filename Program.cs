57007e657a79a2cbe9b07d782458664e8232af19
KFL <kfldev@outlook.com>
2018-12-29 23:02:52 -0800

    .

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cs0649_vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foo = new Foo();
+            foo.X = "hi";
+            Console.WriteLine(foo.X);
+        }
+
+        public class Foo
+        {
+            public string X;
+            public string Y;
+        }
+    }
+}
